cff6989bea51599761c811b711265b3037dcff8d
Peter Makoi <31699860+pkjuak@users.noreply.github.com>
2020-10-20 11:13:34 +0200

    Update FullBgSuppport.cs

--- FullBgSuppport.cs
+++ FullBgSuppport.cs
@@ -2,8 +2,8 @@ public void GeneratePdf(string src, string dest)
 {
     //A converter properties with a base uri set
     // to point to the directory where images can be retrieved
-    var pdfDocument = new PdfDocument(new PdfWriter(dest));
     var properties = new ConverterProperties().SetBaseUri(RESOURCES_DIR);
+    var pdfDocument = new PdfDocument(new PdfWriter(dest));
     var fileInputStream = new FileStream(src, FileMode.Open);
     HtmlConverter.ConvertToPdf(fileInputStream, pdfDocument, properties);
 }
