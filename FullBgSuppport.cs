12ee978d5a46084b090c373f046a8f20125f33cb
Peter Makoi <31699860+pkjuak@users.noreply.github.com>
2020-10-20 11:05:16 +0200

    Create FullBgSuppport.cs

---
+++ FullBgSuppport.cs
@@ -0,0 +1,9 @@
+public void GeneratePdf(string src, string dest)
+{
+    //A converter properties with a base uri set
+    // to point to the directory where images can be retrieved
+    var pdfDocument = new PdfDocument(new PdfWriter(dest));
+    var properties = new ConverterProperties().SetBaseUri(RESOURCES_DIR);
+    var fileInputStream = new FileStream(src, FileMode.Open);
+    HtmlConverter.ConvertToPdf(fileInputStream, pdfDocument, properties);
+}
