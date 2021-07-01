c2e4356c26104aff803a159f1d4d168075982c9e
Peter Makoi <31699860+pkjuak@users.noreply.github.com>
2021-07-01 16:51:32 +0200

    Update Type3fonts.cs

--- Type3fonts.cs
+++ Type3fonts.cs
@@ -1,10 +1,25 @@
- private  static void ManipulatePdf(string src, string dest)
+ using System.Collections.Generic;
+using iText.Forms;
+using iText.Kernel.Colors;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.License;
+using iText.PdfCleanup;
+
+namespace TYPE3TEST
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
         {
             //Load the license file to use cleanup features
             LicenseKey.LoadLicenseFile("license.xml");
-            using (var pdfDocument = new PdfDocument(new PdfReader(src), new PdfWriter(dest)))
+            var properties = new StampingProperties();
+            using (var pdfDocument =
+                new PdfDocument(new PdfReader("base.pdf"),
+                    new PdfWriter("cleanupResult.pdf"), properties))
             {
-               
+
                 var form = PdfAcroForm.GetAcroForm(pdfDocument, false);
                 form?.FlattenFields();
 
@@ -17,3 +32,5 @@
                 cleaner.CleanUp();
             }
         }
+    }
+}
